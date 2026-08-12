4289c124bd914aa9b09c5515f7a1b7f8f73f48b0
Rahim10020 <rahialighi@gmail.com>
2026-08-12 15:40:02 +0000

    fixes

--- _packages/core/lib/fluttercheck_core.dart
+++ _packages/core/lib/fluttercheck_core.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
-
 export 'src/models/analysis_result.dart';
 export 'src/models/constraint.dart';
 export 'src/models/dependency.dart';
